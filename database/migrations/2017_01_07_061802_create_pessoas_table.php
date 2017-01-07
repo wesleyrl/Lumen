d10bf6ca4650320d82b71ea601e9ef6e7007e06b
wesley <wesleyrl.21@gmail.com>
2017-01-07 08:30:57 -0200

    cursoLumen migrates

---
+++ database/migrations/2017_01_07_061802_create_pessoas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('nome')->unique();
+            $table->string('apelido',50);
+            $table->enum('sexo',['F','M']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
